b9fc77f70ad6a5082e16e28bbf052a975db2c8a5
Sumit Maji <skmaji1@outlook.com>
2018-06-08 09:16:31 +0530

    Removed console logs

--- githook/server/index.js
+++ githook/server/index.js
@@ -17,12 +17,12 @@ function verifyPostData(req, res, next){
   if(!payload){
     return next('Request body empty')
   }
-  console.log(process.env.GITHUB_SECRET)
+
   const hmac = crypto.createHmac('sha1',process.env.GITHUB_SECRET);
   const digest = 'sha1=' + hmac.update(payload).digest('hex');
-  console.log('Digest', digest);
+
   const checksum = req.headers[SIGNATURE];
-  console.log('checksum', checksum);
+  
   if(!checksum || !digest || checksum !== digest){
     return next(`Request body digest (${digest}) didnot match ${SIGNATURE} (${checksum})`)
   }
