48586cce56d2d3511d0d744bae784206544d5e0c
Sumit Maji <skmaji1@outlook.com>
2018-06-10 20:26:52 +0530

    Added bash script

--- dockerbuild/run.sh
+++ dockerbuild/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker stop docker-build
-docker rm docker-build
-docker run -v /var/run/docker.sock:/var/run/docker.sock -p 5002:5002 -d --name docker-build sumit/docker-build-app
+source config
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker run -v /var/run/docker.sock:/var/run/docker.sock -p 5002:5002 -d --name $CONTAINER_NAME sumit/$REPO_NAME
