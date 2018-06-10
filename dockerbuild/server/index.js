f9e396e91ce9eb62b1b640f45d0c70e914441220
Sumit Maji <skmaji1@outlook.com>
2018-06-10 18:59:06 +0530

    Commit

--- dockerbuild/server/index.js
+++ dockerbuild/server/index.js
@@ -4,24 +4,43 @@ const app = express();
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const shell = require('shelljs')
+const Promise = require('bluebird')
 
 //To parse URL encoded data
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 //To parse json data
 app.use(bodyParser.json());
 
 app.post('/process', (req, res, next) => {
-    try{
+  try {
 
-      var url = req.body.repository.owner.html_url;
-      var rep = req.body.repository.name;
-      console.log(url, rep)
-      shell.exec(`./scripts/build.sh -r ${rep}.git -u ${url}`)
-      res.status(200).send('done');
-    }catch(err){
-        next(err);
-    }
+    var url = req.body.repository.owner.html_url;
+    var rep = req.body.repository.name;
+    console.log(url, rep)
+    shell.exec()
+    execAsync(`./scripts/build.sh -r ${rep}.git -u ${url}`, {
+        silent: true,
+        cwd: '.'
+      })
+      .then(stdout => console.log('Success'))
+      .catch(err => console.log(err));
+    res.status(200).send('done');
+  } catch (err) {
+    next(err);
+  }
 });
+
+function execAsync(cmd, opts = {}) {
+  return new Promise(function(resolve, reject) {
+    // Execute the command, reject if we exit non-zero (i.e. error)
+    shell.exec(cmd, opts, function(code, stdout, stderr) {
+      if (code != 0) return reject(new Error(stderr));
+      return resolve(stdout);
+    });
+  });
+}
 
 
 app.use((err, req, res, next) => {
