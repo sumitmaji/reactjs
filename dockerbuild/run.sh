a23c580796cc34d4f7d6dc660ec5602b05a15299
Sumit Maji <skmaji1@outlook.com>
2018-06-10 17:47:25 +0530

    Added docker sock socket

--- dockerbuild/run.sh
+++ dockerbuild/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop docker-build
 docker rm docker-build
-docker run -p 5002:5002 -d --name docker-build sumit/docker-build-app
+docker run -v /var/run/docker.sock:/var/run/docker.sock -p 5002:5002 -d --name docker-build sumit/docker-build-app
