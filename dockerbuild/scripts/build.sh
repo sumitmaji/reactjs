68a7b140d070b68287930367311f12317405175e
Sumit Maji <skmaji1@outlook.com>
2018-06-10 13:17:41 +0530

    Clone git repository

---
+++ dockerbuild/scripts/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+[[ "TRACE" ]] && set -x
+
+: ${BRANCH:=master}
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+ -r|--repository)
+ REP="$2"
+ shift
+ shift
+ ;;
+ -u|--url)
+ URL="$2"
+ shift
+ shift
+ ;;
+ -b|--branch)
+ BRANCH="$2"
+ shift
+ shift
+ ;;
+
+esac
+done
+
+if [[ -z $REP ]]; then
+  echo "Please provide repository name."
+  exit 1
+elif [[ -z $URL ]]; then
+  echo "Please provide the url (e.g. https://github.com/sumitmaji)."
+  exit 1
+fi
+
+NUMBER=$[ ( $RANDOM % 100 )  + 1 ]
+mkdir /tmp/$NUMBER
+push /tmp/$NUMBER
+git clone -b $BRANCH $URL/$REP
+pop /tmp/$NUMBER
