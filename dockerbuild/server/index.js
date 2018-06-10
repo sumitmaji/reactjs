c9ebda763337d1a45b752dcd6e9f3c1f12273b46
Sumit Maji <skmaji1@outlook.com>
2018-06-10 18:36:21 +0530

    Commit

--- dockerbuild/server/index.js
+++ dockerbuild/server/index.js
@@ -16,7 +16,7 @@ app.post('/process', (req, res, next) => {
       var url = req.body.repository.owner.html_url;
       var rep = req.body.repository.name;
       console.log(url, rep)
-      //shell.exec(`../scripts/build.sh -r ${rep} -u ${url}`)
+      shell.exec(`../scripts/build.sh -r ${rep}.git -u ${url}`)
       res.status(200).send('done');
     }catch(err){
         next(err);
