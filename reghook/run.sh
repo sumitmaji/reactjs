79f1f219686b90fd6069b3505dd57e2d2f68f009
root <root@master.cloud.com>
2018-06-14 06:32:07 +0530

    Updated reghook

--- reghook/run.sh
+++ reghook/run.sh
@@ -4,4 +4,4 @@ source scripts/config
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $BUILD_PATH:/tmp -p 5002:5002 -d --name $CONTAINER_NAME sumit/$REPO_NAME
+docker run -v ~/.kube:/root/.kube -v $BUILD_PATH:/tmp -p 5002:5002 -d --name $CONTAINER_NAME $REPO_NAME
